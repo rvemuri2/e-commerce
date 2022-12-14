98296ad43e7f01d01fb0295439b7a371fd03323c
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-14 00:55:36 -0800

    Added Logo, routes, navigation, and a sign-in page for the application

---
+++ src/routes/home/home-component.jsx
@@ -0,0 +1,40 @@
+import "../../components/categories/categories.styles.scss";
+import Category from "../../components/categories/categories-component.jsx";
+import { Outlet } from "react-router-dom";
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+
+  return (
+    <div>
+      <Outlet />
+      <Category categories={categories} />
+    </div>
+  );
+};
+export default Home;
