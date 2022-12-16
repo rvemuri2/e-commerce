67aafdacbf7b19756e60be91c5cdb44a011705a4
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-16 02:59:20 -0800

    Added a cart icon so that users can checkout their items

--- src/index.js
+++ src/index.js
@@ -6,14 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./contexts/user.context.jsx";
 import { ProductsProvider } from "./contexts/products.context.jsx";
-
+import { CartProvider } from "./contexts/cart.context.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
