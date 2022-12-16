808a88dd3f4a6a7f6a309d18f3ebc62ae7cd5c89
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-16 02:25:02 -0800

    added shop data that are clothing images, prices, and names and mapped them out in the shop section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import Home from "./routes/home/home-component.jsx";
 import Navigation from "./routes/navigation/navigation.component.jsx";
 import { Routes, Route } from "react-router-dom";
 import Authentication from "./routes/authentication/authentication.component.jsx";
+import Shop from "./routes/shop/shop.component.jsx";
 
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
 const App = () => {
   return (
     <Routes>
