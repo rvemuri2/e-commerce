07d2ba1979184f0318bd2d04b1b4aee8d9d794b6
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-16 21:51:15 -0800

    enabled user to be able to add items to the cart-dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import Button from "../button/button.component";
 
 import "./cart-dropdown.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () => (
-  <div className="cart-dropdown-container">
-    <div className="cart-items" />
-    <Button>GO TO CHECKOUT</Button>
-  </div>
-);
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
