515e4f0c67a9e9f350bb31e2c62ad41a994984af
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-17 20:48:01 -0800

    added database for clothing in firebase, accessed firebase using map and contexts. Then, made preview feature for all types of clothing.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./contexts/user.context.jsx";
-import { ProductsProvider } from "./contexts/products.context.jsx";
+import { CategoriesProvider } from "./contexts/categories.context.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
