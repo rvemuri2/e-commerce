ae11c53561eaf77ac7e5d463e3c2fe2a079399ea
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-21 02:15:43 -0800

    setting up Stripe API to handle online test payments

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { store, persistor } from "./store/store";
 
 import "./index.scss";
 import { PersistGate } from "redux-persist/integration/react";
-
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 const rootElement = document.getElementById("root");
 
 render(
@@ -16,7 +17,9 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
