92c5292dcadececdf8885106397738b2ef3c78a8
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-26 23:07:30 -0800

    put button component back as jsx

---
+++ src/components/button/button.component.jsx
@@ -0,0 +1,23 @@
+//three types of button: inverted, default, google sign in
+import { FC, ButtonHTMLAttributes } from "react";
+import "./button.styles.scss";
+export const BUTTON_TYPE_CLASSES = {
+  google: "google-sign-in",
+  inverted: "inverted",
+  base: "base",
+};
+export type ButtonProps = {
+  buttonType?: BUTTON_TYPE_CLASSES,
+  isLoading?: boolean,
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+const Button: FC<ButtonProps> = ({ children, buttonType, ...otherProps }) => {
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
+export default Button;
