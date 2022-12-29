88cc8f63e423500f392473328fc618dc05d61bb8
Rohit Vemuri <rohitvemuri@yahoo.com>
2022-12-29 02:23:49 -0800

    fixed up button styling

--- src/components/sign-in-form/sign-in-form.styles.tsx
+++ src/components/sign-in-form/sign-in-form.styles.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SignInContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 380px;
+  width: 450px;
   h2 {
     margin: 10px 0;
   }
